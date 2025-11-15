dcf667c70580e65872daa044eec87f91855c3eb9
HopScotcher <brianndolo846@gmail.com>
2025-11-15 13:36:08 +0300

    added quize game v1

--- QuizGame/Program.cs
+++ QuizGame/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// using System;
+
+// namespace QuizGame{
+//     class Program{
+
+//         static void QuizMaster()
+//         {
+
+//          Dictionary<string, string> quiz = new Dictionary<string, string>
+//         {
+//             {"whats 1+1?", "2"},
+//             {"whats 2+2?", "4"},
+//             {"whats 4+4?", "8"},
+//         };
+
+//         int score=0;
+
+//             foreach(var item in quiz)
+//         {
+//             Console.WriteLine(item.Key);
+//             string attempt = Console.ReadLine();
+//             score = attempt == item.Value? score+=10 : 0;
+//         }
+
+//         Console.WriteLine($"Your score: {score}");
+//         }
+//         static void Main(){
+//             Console.WriteLine("----Welcome to quiz game-------");
+//             QuizMaster();
+//         }
+//     }
+// }
+
+
+
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+    
+
+        static void Main()
+        {
+            // Lambda
+            // Func<int, int, int> add = (x,y) => x+y;
+
+            // Action greeting = () => Console.WriteLine("ehllo");
+
+            // int result = add(2,3);
+ 
+                
+        }               
+    }
+}

8ddebd636fac658573c1b126d913cdbca885fb16
HopScotcher <brianndolo846@gmail.com>
2025-11-18 01:02:42 +0300

    added Contact manager mini console program

---
+++ ContactManager/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ContactManager
+{
+    class Program
+    {
+
+        abstract class Contact
+        {
+            private string name{get;set;}
+            private int phoneNo{get;set;}
+
+            public abstract void GetDetails()
+            {
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--WELCOME TO SIMPLE CONTACT MANAGER---");
+
+
+        }
+    }
+}
